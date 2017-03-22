7d2f37da9f457eef2c0446eaefda2d30bc75e205
guolifeng <glf_work@163.com>
2017-03-22 20:25:59 +0800

    Initial commit

---
+++ app/src/main/java/com/guolifeng/android/servicetest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.guolifeng.android.servicetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
