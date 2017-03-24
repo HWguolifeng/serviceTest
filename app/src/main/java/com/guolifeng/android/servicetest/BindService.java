7b25884af5c604012658ed5771632945e0cb6c9a
guolifeng <glf_work@163.com>
2017-03-24 19:34:39 +0800

    2:第二种启动方式

---
+++ app/src/main/java/com/guolifeng/android/servicetest/BindService.java
@@ -0,0 +1,83 @@
+package com.guolifeng.android.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BindService extends Service
+{
+    private static final String TAG = "BindService";
+    private int count;
+    private boolean quit;
+    private MyBinder binder = new MyBinder();
+
+    public BindService()
+    {
+        Log.i(TAG, "BindService: ");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        Log.i(TAG, "onBind: ");
+        return binder;
+    }
+
+    class MyBinder extends Binder
+    {
+        int getCount()
+        {
+            return count;
+        }
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Log.i(TAG, "onCreate: ");
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                while (!quit)
+                {
+                    try
+                    {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e)
+                    {
+                        Log.i(TAG, "run: e");
+                    }
+                    count++;
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent)
+    {
+        Log.i(TAG, "onUnbind: ");
+        return true;
+
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        quit = true;
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Log.i(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
--- app/src/main/java/com/guolifeng/android/servicetest/MainActivity.java
+++ app/src/main/java/com/guolifeng/android/servicetest/MainActivity.java
@@ -1,29 +1,57 @@
 package com.guolifeng.android.servicetest;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity
 {
 
-    Context context;
-    Intent intent;
+    private static final String TAG = "MainActivity";
+    private Context context;
+
+    private BindService.MyBinder myBinder;
+    private boolean isConnection;
+
+    private ServiceConnection serviceConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            Log.i(TAG, "onServiceConnected: ");
+            myBinder = (BindService.MyBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            Log.i(TAG, "onServiceDisconnected: ");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获得View
         Button bindService = (Button) this.findViewById(R.id.bind);
         Button unBindService = (Button) this.findViewById(R.id.unBind);
+        Button getStatus = (Button) this.findViewById(R.id.get_status);
+        //获得上下文
         context = this;
+        //设置View的监听
         MyOnClickListener myOnClickListener = new MyOnClickListener();
         bindService.setOnClickListener(myOnClickListener);
         unBindService.setOnClickListener(myOnClickListener);
+        getStatus.setOnClickListener(myOnClickListener);
 
     }
 
@@ -36,18 +64,38 @@ public class MainActivity extends Activity
         @Override
         public void onClick(View v)
         {
-            intent = new Intent(context, FirstService.class);
+            Intent intent = new Intent(context, BindService.class);
             switch (v.getId())
             {
                 case R.id.bind:
-                    startService(intent);
+                    bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+                    isConnection = true;
                     break;
                 case R.id.unBind:
-                    stopService(intent);
+                    if (isConnection)
+                    {
+                        unbindService(serviceConnection);
+                        isConnection = false;
+                    }
+
+                    break;
+                case R.id.get_status:
+                    getCount();
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    private int getCount()
+    {
+        int count = 0;
+        if (myBinder != null)
+        {
+            count = myBinder.getCount();
+        }
+        Log.i(TAG, "getCount: " + count);
+        return count;
     }
 }
